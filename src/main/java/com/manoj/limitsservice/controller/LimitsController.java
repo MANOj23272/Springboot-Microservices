d753338e9f53c51ceeaaa744d616b3dbbb1796c9
udayk <panjinimanojkumar@gmail.com>
2023-04-20 22:44:39 +0530

    first commit

---
+++ src/main/java/com/manoj/limitsservice/controller/LimitsController.java
@@ -0,0 +1,21 @@
+package com.manoj.limitsservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.manoj.limitsservice.Configuration.Configuration;
+import com.manoj.limitsservice.bean.Limits;
+
+@RestController
+public class LimitsController {
+
+	@Autowired
+	private Configuration configuration;
+	@GetMapping("/limits")
+	public Limits retriveLimits() {
+		return new Limits(configuration.getMinimum(),configuration.getMaximum());
+		
+		//return new Limits(1,1000);
+	}
+}
